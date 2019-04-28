0ab553eb32ee256c2ce72b4e7f164402224695df
Kurapov75 <kurapov1975@gmail.com>
2019-04-28 20:43:13 +1100

    Исправление ошибок Тревиса

--- source/js/map.js
+++ source/js/map.js
@@ -5,4 +5,3 @@ var searchMapGoogle = document.querySelector('.search__map');
 searchMapGoogle.classList.remove('search__map--nojs');
 searchMap.classList.add('search__map--nojs');
 searchMapMarker.classList.add('search__map--nojs');
-
