bca09b1f23616e7f0a2dce95084df44c1de2d715
Kurapov75 <kurapov1975@gmail.com>
2019-04-28 15:34:36 +1100

    Исправление работы меню на страницах галереи и формы

---
+++ source/js/map.js
@@ -0,0 +1,8 @@
+var searchMap = document.querySelector('.search__map-img');
+var searchMapMarker = document.querySelector('.search__map-marker');
+var searchMapGoogle = document.querySelector('.search__map');
+
+searchMapGoogle.classList.remove('search__map--nojs');
+searchMap.classList.add('search__map--nojs');
+searchMapMarker.classList.add('search__map--nojs');
+
--- source/js/menu.js
+++ source/js/menu.js
@@ -1,16 +1,10 @@
 var navToggle = document.querySelector('.main-nav__toggle');
 var navClose = document.querySelector('.main-nav__close');
 var navMain = document.querySelector('.main-nav');
-var searchMap = document.querySelector('.search__map-img');
-var searchMapMarker = document.querySelector('.search__map-marker');
-var searchMapGoogle = document.querySelector('.search__map');
 var logoTop = document.querySelector('.page-hdr__logo-img');
 
 navToggle.classList.remove('main-nav__toggle--nojs');
 navClose.classList.remove('main-nav__close--nojs');
-searchMapGoogle.classList.remove('search__map--nojs');
-searchMap.classList.add('search__map--nojs');
-searchMapMarker.classList.add('search__map--nojs');
 
 navClose.addEventListener('click', function() {
   navMain.classList.add('main-nav--closed')
